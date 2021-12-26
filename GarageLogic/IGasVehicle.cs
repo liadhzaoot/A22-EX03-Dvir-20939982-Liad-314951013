481723e9f73abc1da99a3b7859f646b0a6ed5502
Dvir YT <86402614+DVIRO44@users.noreply.github.com>
2021-12-26 22:56:04 +0200

    Gas Vechicle

---
+++ GarageLogic/IGasVehicle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarageLogic
+{
+
+
+    interface IGasVehicle
+    {
+        public enum eGasType
+        {
+            Octan95,
+            Octan96,
+            Octan98,
+            Soler
+        }
+        float CurrentLiterGasCapacity { get; set; }
+
+        float MaxLiterGasCapacity { get; set; }
+
+        void AddGas(float i_GasAmountToAdd, );
+
+    }
+}
