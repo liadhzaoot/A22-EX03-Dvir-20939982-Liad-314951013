0831a571d7b39efc95bd52f4d12db6063a2c6a43
Liad Hazoot <liadhazoot5@gmail.com>
2021-12-26 22:54:27 +0200

    elecrtric vehicle + enumclass + grage

---
+++ GarageLogic/EnumClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarageLogic
+{
+    public class EnumClass
+    {
+        public class GasTypeEnum
+        {
+            public enum eGasType
+            {
+                Octan98,
+                Octan96,
+            }
+        }
+    }
+}
---
+++ GarageLogic/Garage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarageLogic
+{
+    class Garage
+    {
+        private string m_OwnerName;
+        private string m_OwnerPhoneNumber;
+        public enum wnerName;
+    }
+}
